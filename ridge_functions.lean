81faba18a44929cd781d6c0e510993c58a581e9c
Leorasz <140349854+Leorasz@users.noreply.github.com>
2025-12-31 16:02:41 -0700

    Update ridge_functions.lean

--- ridge_functions.lean
+++ ridge_functions.lean
@@ -55,7 +55,6 @@ theorem trivial_annihilator_implies_eq (Ω : Finset (Fin n → ℝ)) (m : ℕ)
       exact trivial
     exact this
   refine Submodule.span_le.mpr fun g hg => ?_
-  simp only [Set.mem_setOf_eq] at hg
   obtain ⟨a, rfl⟩ := hg
   let q : MvPolynomial (Fin n) ℝ := ∑ i : Fin n, MvPolynomial.C (a.val i) * MvPolynomial.X i
   have hq : q.IsHomogeneous 1 := by
@@ -67,13 +66,10 @@ theorem trivial_annihilator_implies_eq (Ω : Finset (Fin n → ℝ)) (m : ℕ)
     simp [p]
     nth_rewrite 2 [← one_mul m]
     exact MvPolynomial.IsHomogeneous.pow hq m
-  have hp_mem : p ∈ MvPolynomial.homogeneousSubmodule (Fin n) ℝ m := by
-    exact hp
   have heq : (polyHom K) p = ridgeFunction K Ω a ⟨fun x => x ^ m, continuous_pow m⟩ := by
     ext x
-    simp only [ridgeFunction, ContinuousMap.coe_mk, polyHom]
-    simp [p, q, coord]
-  exact heq ▸ Submodule.mem_map_of_mem hp_mem
+    simp [ridgeFunction, ContinuousMap.coe_mk, polyHom, p, q, coord]
+  exact heq ▸ Submodule.mem_map_of_mem hp
 
 def coords : Set C(K, ℝ) := Set.range (coord K)
 
@@ -91,23 +87,19 @@ def ridges (Ω : Finset (Fin n → ℝ)) : Submodule ℝ C(↑K, ℝ) :=
   Submodule.span ℝ {g | ∃ (a : Ω) (f : C(ℝ, ℝ)), g = ridgeFunction K Ω a f}
 
 --showing you can make polynomials out of ridge functions by using what we have for homogeneous polynomials
+lemma mono_submod_ridges (Ω : Finset (Fin n → ℝ)) (m : ℕ) : monoRidges K Ω m ≤ ridges K Ω := by
+  apply Submodule.span_mono
+  intro g hg
+  simp only [Set.mem_setOf_eq] at hg
+  obtain ⟨a, f, rfl⟩ := hg
+  exact ⟨a, ⟨(· ^ m), continuous_pow m⟩, rfl⟩
+
 theorem homogeneous_ridged_implies_polynomials (Ω : Finset (Fin n → ℝ))
   (h : ∀(m : ℕ), Submodule.map (polyHom K).toLinearMap (MvPolynomial.homogeneousSubmodule (Fin n) ℝ m) = monoRidges K Ω m) :
   (polySubalg K).toSubmodule ≤ ridges K Ω := by
   rw [polySubalg_eq_range_aeval]
-  intro f hf
-  obtain ⟨p, rfl⟩ := hf
-  rw [← MvPolynomial.sum_homogeneousComponent p]
-  have mono_submod_ridges (m : ℕ) : monoRidges K Ω m ≤ ridges K Ω := by
-    apply Submodule.span_mono
-    intro g hg
-    simp only [Set.mem_setOf_eq] at hg
-    obtain ⟨a, f, rfl⟩ := hg
-    exact ⟨a, ⟨(· ^ m), continuous_pow m⟩, rfl⟩
-  have h_map_sum : (polyHom K).toRingHom (∑ i ∈ Finset.range (p.totalDegree + 1), (MvPolynomial.homogeneousComponent i) p) =
-    ∑ i ∈ Finset.range (p.totalDegree + 1), (polyHom K) ((MvPolynomial.homogeneousComponent i) p) := by
-    exact map_sum (polyHom K).toRingHom _ _
-  rw [h_map_sum]
+  rintro f ⟨p, rfl⟩
+  rw [← MvPolynomial.sum_homogeneousComponent p, map_sum]
   have h_each_in_ridges (m : ℕ) (hm : m ∈ Finset.range (p.totalDegree + 1)) :
     (polyHom K) ((MvPolynomial.homogeneousComponent m) p) ∈ ridges K Ω := by
     have hmem : (MvPolynomial.homogeneousComponent m) p ∈ MvPolynomial.homogeneousSubmodule (Fin n) ℝ m := by
@@ -116,7 +108,7 @@ theorem homogeneous_ridged_implies_polynomials (Ω : Finset (Fin n → ℝ))
         Submodule.map (polyHom K).toLinearMap (MvPolynomial.homogeneousSubmodule (Fin n) ℝ m) := by
       exact Submodule.mem_map_of_mem hmem
     rw [h m] at this
-    exact mono_submod_ridges m this
+    exact mono_submod_ridges K Ω m this
   exact Submodule.sum_mem (ridges K Ω) h_each_in_ridges
 
 --ridge functions except the 1D function is a polynomial
@@ -126,11 +118,8 @@ def polynomialRidges (Ω : Finset (Fin n → ℝ)) : Submodule ℝ C(↑K, ℝ) 
 
 --shows that polynomial ridges are ridges
 lemma polynomial_ridges_submod_ridges (Ω : Finset (Fin n → ℝ)): polynomialRidges K Ω ≤ ridges K Ω := by
-  rw [polynomialRidges, ridges]
   apply Submodule.span_mono
-  intro g hg
-  simp only [Set.mem_setOf_eq] at hg
-  obtain ⟨a, f, rfl⟩ := hg
+  rintro g ⟨a, f, rfl⟩
   exact ⟨a, ⟨fun t ↦ f.eval t, f.continuous⟩, rfl⟩
 
 variable [CompactSpace K]
@@ -160,15 +149,11 @@ theorem dense_of_contains_polySubalg {S : Submodule ℝ C(↑K, ℝ)}
     exact id (Eq.symm this)
   have h_closure_mono := closure_mono h_subset
   rw [h_dense] at h_closure_mono
-  have h_univ : closure (↑S) = (⊤ : Set C(↑K, ℝ)) := by
-    exact Eq.symm (Set.Subset.antisymm h_closure_mono fun ⦃a⦄ a ↦ trivial)
   exact Submodule.dense_iff_topologicalClosure_eq_top.mp fun x ↦ h_closure_mono trivial
 
 --final theorem, closure of ridge functions in compact space of continuous functions given Ω doesn't give rise to any annihilators
 theorem ridges_closed (Ω : Finset (Fin n → ℝ)) (h_annihilator : ∀m, (monoPoly K Ω m).dualAnnihilator = ⊥) :
   (ridges K Ω).topologicalClosure = ⊤ := by
-  have polynomials : (polySubalg K).toSubmodule ≤ ridges K Ω := by
-    have : ∀(m : ℕ), Submodule.map (polyHom K).toLinearMap (MvPolynomial.homogeneousSubmodule (Fin n) ℝ m) = monoRidges K Ω m := by
-      exact fun m ↦ trivial_annihilator_implies_eq K Ω m (h_annihilator m)
-    exact homogeneous_ridged_implies_polynomials K Ω this
-  exact dense_of_contains_polySubalg K polynomials
+  refine dense_of_contains_polySubalg K ?_
+  refine homogeneous_ridged_implies_polynomials K Ω fun m => ?_
+  exact trivial_annihilator_implies_eq K Ω m (h_annihilator m)
